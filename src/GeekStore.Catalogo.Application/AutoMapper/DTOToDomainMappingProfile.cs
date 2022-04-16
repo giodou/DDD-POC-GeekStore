5a0b7cf14b0f7ea269214d95e19160269cd69cb2
Giovane Timm <giovane.timm@sambatech.com.br>
2022-04-15 23:33:07 -0300

    Criado DTOS, camada de application e configurado automapper para o contexto de catalogo

---
+++ src/GeekStore.Catalogo.Application/AutoMapper/DTOToDomainMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GeekStore.Catalogo.Application.DTO;
+using GeekStore.Catalogo.Domain;
+
+namespace GeekStore.Catalogo.Application.AutoMapper
+{
+    public class DTOToDomainMappingProfile : Profile
+    {
+        public DTOToDomainMappingProfile()
+        {
+            CreateMap<ProdutoDTO, Produto>()
+                .ConstructUsing(p =>
+                    new Produto(p.Nome, p.Descricao, p.Ativo,
+                        p.Valor, p.CategoriaId, p.DataCadastro,
+                        p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+            CreateMap<CategoriaDTO, Categoria>()
+                .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+        }
+    }
+}
---
+++ src/GeekStore.Catalogo.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GeekStore.Catalogo.Application.DTO;
+
+namespace GeekStore.Catalogo.Application.Services
+{
+    public interface IProdutoAppService : IDisposable
+    {
+        Task<IEnumerable<ProdutoDTO>> ObterPorCategoria(int codigo);
+        Task<ProdutoDTO> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoDTO>> ObterTodos();
+        Task<IEnumerable<CategoriaDTO>> ObterCategorias();
+
+        Task AdicionarProduto(ProdutoDTO produtoViewModel);
+        Task AtualizarProduto(ProdutoDTO produtoViewModel);
+
+        Task<ProdutoDTO> DebitarEstoque(Guid id, int quantidade);
+        Task<ProdutoDTO> ReporEstoque(Guid id, int quantidade);
+    }
+}
